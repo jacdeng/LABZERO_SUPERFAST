931947fe9c871e15ad82912ea77c5691c007b8d9
yangsu01 <42984226+yangsu01@users.noreply.github.com>
2019-09-11 20:45:17 -0700

    yang suu

--- lab0.py
+++ lab0.py
@@ -3,7 +3,7 @@ import cv2
 
 cap = cv2.VideoCapture('raw_video_feed.mp4')
 
-# 225 is my bottom row
+# 225 is bottom row
 botrow = 225
 
 while(cap.isOpened()):
@@ -11,24 +11,18 @@ while(cap.isOpened()):
     ret, bnw_frame = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
 
     width = int(cap.get(3))
-    # height = cap.get(4)
 
-    leftwhitespace = 0
-    linespace = 0
-    ontheleft = True
-    ontheright = False
-    
+    on_line = False
+
     for i in range(width):
-        if bnw_frame[ botrow , i , 0 ] == 255 and ontheleft == True:
-            leftwhitespace = leftwhitespace + 1
         if bnw_frame[ botrow , i , 0 ] == 0:
-            linespace = linespace + 1
-            ontheright = True
-            ontheleft = False
+            linestart = i
+            on_line = True
+        if bnw_frame[ botrow , i , 0 ] == 255 and on_line == True:
+            linestop = i - 60
+            on_line = False
 
-    print(leftwhitespace)
-
-    center = leftwhitespace + linespace/2
+    center = (linestop + linestart) / 2
 
     # circle , frame , position , radius , color , thickness (-1 is solid) 
     cv2.circle(frame, (center, 225), 23, (0, 0, 255), -1)
